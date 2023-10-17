a28d6a8d8204ffd5be7f5ff27a375a1e16e622ea
tewtewpemtat <110074190+tewtewpemtat@users.noreply.github.com>
2023-10-18 01:29:37 +0700

    navbar update

--- src/details/experience.js
+++ src/details/experience.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Experience = () => {
-  return <div>Experience Page</div>;
+  return <div style={{ paddingTop: '200px' }}>
+  Experience Page
+</div>
 };
 
 export default Experience;
