d90e14d549f2e56bfd86cd4c8084ed17bd11bc12
tewtewpemtat <110074190+tewtewpemtat@users.noreply.github.com>
2023-10-18 23:23:52 +0700

    success aboutme

--- src/details/aboutme.js
+++ src/details/aboutme.js
@@ -17,18 +17,30 @@ const AboutMe = () => {
     return () => clearInterval(interval);
   }, [currentImage, images.length]);
 
-
   return (
     <div style={{ paddingTop: '70px' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px',marginBottom: '20px' }}>
         <img src={images[currentImage]} alt={`slide ${currentImage + 1}`} style={{ width: '300px', height: '300px', objectFit: 'cover', margin: '0 20px' }} />
       </div>
-      <div>
-        <h2>Hello, I'm Tew</h2>
-        <p>21 years old</p>
-        <p>Currently studying at King Mongkut's University of Technology North Bangkok, majoring in Electronics Engineering, Computer Branch.</p>
-        <p>I have a strong interest in enhancing my skills in web development, mobile app development, database design, front-end, and back-end development. I am passionate about advancing these skills individually or in tandem.</p>
-        <p>I have excellent teamwork and communication skills, which enable me to work effectively with others to produce the best results.</p>
+      <div style={{
+        border: '10px solid #B279D2',
+        padding: '20px', 
+        borderRadius: '10px', 
+        textAlign: 'center', 
+        maxWidth: '600px', 
+        margin: '0 auto', 
+        background: `url('./img/bg3.jpg')`,
+        backgroundSize: 'cover',
+        fontFamily: 'Arial, sans-serif', 
+        boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.1)', 
+        }}>
+        <h2 style={{ color: '#1A237E' }}>Hello, I'm Tew</h2> 
+        <p>
+              21 years old<br/>
+              Currently studying at King Mongkut's University of Technology North Bangkok, majoring in Electronics Engineering, Computer Branch.<br />
+              I have a strong interest in enhancing my skills in web development, mobile app development, database design, front-end, and back-end development. I am passionate about advancing these skills individually or in tandem.<br />
+              I have excellent teamwork and communication skills, which enable me to work effectively with others to produce the best results.
+        </p>
       </div>
     </div>
   );
